0f427461498211c2d19b0143fa7af93ea49bc4cc
Peter Volosenco <petervolosenco@gmail.com>
2023-08-11 17:59:42 +0100

    init

---
+++ src/Posts.vue
@@ -0,0 +1,59 @@
+<script lang="ts">
+import { defineComponent } from "vue";
+import { useQuery } from "@tanstack/vue-query";
+
+import type { Post } from "./types";
+
+const fetcher = async (): Promise<Post[]> => {
+  await new Promise((resolve) => setTimeout(resolve, 1000));
+  return await fetch("https://jsonplaceholder.typicode.com/posts").then(
+    (response) => response.json()
+  );
+};
+
+export default defineComponent({
+  name: "PostsList",
+  props: {
+    isVisited: {
+      type: Function,
+      required: true,
+    },
+  },
+  emits: ["setPostId"],
+  setup() {
+    const { isLoading, isError, isFetching, data, error, refetch } = useQuery({
+      queryKey: ["posts"],
+      queryFn: fetcher,
+      staleTime: Infinity,
+      cacheTime: Infinity,
+    });
+
+    return { isLoading, isError, isFetching, data, error, refetch };
+  },
+});
+</script>
+
+<template>
+  <h1>Posts</h1>
+  <div v-if="isLoading">Loading...</div>
+  <div v-else-if="isError">An error has occurred: {{ error }}</div>
+  <div v-else-if="data">
+    <ul>
+      <li v-for="item in data" :key="item.id">
+        <a
+          @click="$emit('setPostId', item.id)"
+          href="#"
+          :class="{ visited: isVisited(item.id) }"
+          >{{ item.title }}</a
+        >
+      </li>
+    </ul>
+  </div>
+</template>
+
+<style scoped>
+.visited {
+  font-weight: bold;
+  color: green;
+}
+</style>
